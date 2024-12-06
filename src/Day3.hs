9e6296262edf9d577a44c7f0daa0d778725f8785
Arthur Bacci <arthurbacci@protonmail.com>
2024-12-06 19:16:39 -0300

    day 3 both parts

--- src/Day3.hs
+++ src/Day3.hs
@@ -6,38 +6,62 @@ module Day3
 
 import Data.Text (Text)
 import Data.Void (Void)
-import Data.Maybe (fromJust)
+import Data.Maybe (catMaybes)
+import Data.Functor (($>))
 import Control.Monad (void)
-import Text.Megaparsec (Parsec, parseTest, anySingle, many, (<|>), try, parseMaybe, takeRest)
+import Control.Monad.Trans.State (State, modify, get, runState)
+import Control.Monad.Trans.Class (lift)
+import Text.Megaparsec hiding (State)
 import Text.Megaparsec.Char (string)
 import Text.Megaparsec.Char.Lexer (decimal)
 
--- FIXME: remove after parsing is working
-import System.IO.Unsafe (unsafePerformIO)
+data Action = AlwaysDo | Do | Don't
+  deriving Eq
 
-type Parser = Parsec Void Text
+type Parser = ParsecT Void Text (State Action)
 
 day3 :: Text -> (String, String)
---day3 d = unsafePerformIO $ do
---  parseTest parser d
---  return ("", "")
-day3 d = (show $ part1 parsed, "")
-  where parsed = fromJust $ parseMaybe parser d
+day3 d = (show $ processParsing AlwaysDo d, show $ processParsing Do d)
 
-part1 :: [(Integer, Integer)] -> Integer
-part1 = sum . fmap (uncurry (*))
+processParsing :: Action -> Text -> Integer
+processParsing a d = case runState (runParserT parser "" d) a of
+  (Right x, _) -> multiply x
+  (Left _, _) -> error "parsing issue"
+
+multiply :: [(Integer, Integer)] -> Integer
+multiply = sum . fmap (uncurry (*))
+
+swapAction :: Action -> Action
+swapAction AlwaysDo = AlwaysDo
+swapAction Do = Don't
+swapAction Don't = Do
 
 parser :: Parser [(Integer, Integer)]
-parser = many findMul <* takeRest
-
-findMul :: Parser (Integer, Integer)
-findMul = parseMul <|> (try $ anySingle >> findMul)
+parser = fmap catMaybes $ many
+       $ fmap (Just) parseMul <|> parseSwap $> Nothing <|> anySingle $> Nothing
 
 parseMul :: Parser (Integer, Integer)
 parseMul = try $ do
+  failIfDon't
   void $ string "mul("
   num1 <- decimal
   void $ string ","
   num2 <- decimal
   void $ string ")"
   return (num1, num2)
+
+parseSwap :: Parser ()
+parseSwap = do
+  a <- lift get
+  case a of
+    AlwaysDo -> fail ""
+    Do       -> void $ string "don't()"
+    Don't    -> void $ string "do()"
+  lift $ modify swapAction
+
+failIfDon't :: Parser ()
+failIfDon't = do
+  a <- lift get
+  if (a == Don't) then fail ""
+  else return ()
+
