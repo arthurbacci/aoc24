4db4018cd2ee32bf69968cbeaeecce973102b1eb
Arthur Bacci <arthurbacci@protonmail.com>
2024-12-07 14:45:07 -0300

    cleaner day 7

--- src/Day7.hs
+++ src/Day7.hs
@@ -6,8 +6,7 @@ import Data.Text (Text)
 import Data.Void
 import Control.Monad
 import Data.Maybe
-import Data.List (find)
-import Data.List.Index
+import Data.List (foldl')
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer
@@ -27,33 +26,25 @@ parts :: Part -> [(Integer, [Integer])] -> Integer
 parts p = sum . fmap (\(t, d) -> if test p (t, d) then t else 0)
 
 test :: Part -> (Integer, [Integer]) -> Bool
-test p (t, d) = isJust $ find (== t) $ listTests p d
-
-listTests :: Part -> [Integer] -> [Integer]
-listTests p d = fmap (\x -> doOps d x) $ f $ length d
+test p (t, d) = any (\o -> testCombinations o t d) $ f $ length d
   where f = case p of
-              One -> getOpCombinations
-              Two -> get2OpComb
+              One -> getAllCombinations [Add, Mul]
+              Two -> getAllCombinations [Add, Mul, Concat]
 
-doOps :: [Integer] -> [Op] -> Integer
-doOps d o = ifoldl (\a -> \i -> \x -> doOp (o !! i) a x) 0 d
+testCombinations :: [Op] -> Integer -> [Integer] -> Bool
+testCombinations o t d = (foldl' f 0 $ zip o d) == t
+  where f a (op, x) = if a > t then a
+                               else doOp op a x
 
 doOp :: Op -> Integer -> Integer -> Integer
 doOp Mul = (*)
 doOp Add = (+)
 doOp Concat = \a -> \b -> read $ show a ++ show b
 
-getOpCombinations :: Int -> [[Op]]
-getOpCombinations 0 = []
-getOpCombinations 1 = [[Mul], [Add]]
-getOpCombinations n = fmap (\a -> [Mul] ++ a) r ++ fmap (\a -> [Add] ++ a) r
-  where r = getOpCombinations (n - 1)
-
-get2OpComb :: Int -> [[Op]]
-get2OpComb 0 = []
-get2OpComb 1 = [[Mul], [Add], [Concat]]
-get2OpComb n = fmap (f Mul) r ++ fmap (f Add) r ++ fmap (f Concat) r
-  where r = get2OpComb (n - 1)
+getAllCombinations :: [Op] -> Int -> [[Op]]
+getAllCombinations _ 0 = [[]]
+getAllCombinations x n = join $ fmap (\o -> fmap (f o) r) x
+  where r = getAllCombinations x (n - 1)
         f o a = [o] ++ a
 
 parser :: Parser [(Integer, [Integer])]
