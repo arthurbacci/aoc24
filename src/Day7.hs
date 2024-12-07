8adced946efe40b0ed2e61686c76877ba93dd950
Arthur Bacci <arthurbacci@protonmail.com>
2024-12-07 15:54:05 -0300

    four times faster day 7

--- src/Day7.hs
+++ src/Day7.hs
@@ -6,7 +6,8 @@ import Data.Text (Text)
 import Data.Void
 import Control.Monad
 import Data.Maybe
-import Data.List (foldl')
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer
@@ -14,9 +15,6 @@ import Text.Megaparsec.Char.Lexer
 type Parser = Parsec Void Text
 
 data Part = One | Two
-
-data Op = Mul | Add | Concat
-  deriving Show
 
 day7 :: Text -> (String, String)
 day7 d = (show $ parts One parsed, show $ parts Two parsed)
@@ -26,26 +24,21 @@ parts :: Part -> [(Integer, [Integer])] -> Integer
 parts p = sum . fmap (\(t, d) -> if test p (t, d) then t else 0)
 
 test :: Part -> (Integer, [Integer]) -> Bool
-test p (t, d) = any (\o -> testCombinations o t d) $ f $ length d
-  where f = case p of
-              One -> getAllCombinations [Add, Mul]
-              Two -> getAllCombinations [Add, Mul, Concat]
+test p (t, d) = Set.size (Set.filter (== t) $ findOutcomes p t d) > 0
 
-testCombinations :: [Op] -> Integer -> [Integer] -> Bool
-testCombinations o t d = (foldl' f 0 $ zip o d) == t
-  where f a (op, x) = if a > t then a
-                               else doOp op a x
+outcomes :: Part -> Integer -> Integer -> Set Integer
+outcomes p a b = case p of
+                   One -> Set.fromList [a * b, a + b]
+                   Two -> Set.fromList [a * b, a + b, read $ show a ++ show b]
 
-doOp :: Op -> Integer -> Integer -> Integer
-doOp Mul = (*)
-doOp Add = (+)
-doOp Concat = \a -> \b -> read $ show a ++ show b
+filteredOutcomes :: Part -> Integer -> Integer -> Integer -> Set Integer
+filteredOutcomes p t a b = Set.filter (<= t) $ outcomes p a b
 
-getAllCombinations :: [Op] -> Int -> [[Op]]
-getAllCombinations _ 0 = [[]]
-getAllCombinations x n = join $ fmap (\o -> fmap (f o) r) x
-  where r = getAllCombinations x (n - 1)
-        f o a = [o] ++ a
+findOutcomes :: Part -> Integer -> [Integer] -> Set Integer
+findOutcomes p t (a:(b:xs)) = Set.unions
+                            $ Set.map (\x -> findOutcomes p t (x:xs)) outs
+  where outs = filteredOutcomes p t a b
+findOutcomes _  _ l = Set.fromList l
 
 parser :: Parser [(Integer, [Integer])]
 parser = many $ do
